cbe712c013361304b72b53246b0b6c065d898f0b
SokIsKedu <et88568@gmail.com>
2017-03-26 20:18:48 +0300

    new app with simple api added

---
+++ polls/models.py
@@ -0,0 +1,40 @@
+from __future__ import unicode_literals
+
+from time import timezone
+from django.contrib.auth.models import User, Group
+from rest_framework import serializers
+
+import datetime
+from django.db import models
+
+
+class Question(models.Model):
+    question_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date published')
+
+    def __str__(self):  # __unicode__ on Python 2
+        return self.question_text
+
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+
+
+class Choice(models.Model):
+    question = models.ForeignKey(Question)
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
+
+    def __str__(self):  # __unicode__ on Python 2
+        return self.choice_text
+
+
+class UserSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = User
+        fields = ('url', 'username', 'email', 'groups')
+
+
+class GroupSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Group
+        fields = ('url', 'name')
